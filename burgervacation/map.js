90bd1059f7cf400d6ac28cd9095925989664473c
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-11-17 21:04:11 -0800

    add google map

--- burgervacation/map.js
+++ burgervacation/map.js
@@ -1,9 +1,65 @@
 function initialize() {
        	var latlng = new google.maps.LatLng(49.2850818,-123.1154072);
      	var myOptions = {
-		zoom: 18, /*拡大比率*/
-	    	center: latlng, /*表示枠内の中心点*/
-		mapTypeId: google.maps.MapTypeId.ROADMAP/*表示タイプの指定*/
+		zoom: 18,
+	    	center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
    	var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+	
+	// style
+	var styleOptions = [
+	{
+		featureType: "all",
+	     	elementType: "all",
+	   	stylers: [
+			{ saturation: -100 }
+	     	] 
+  	},{
+		featureType: "all",
+		elementType: "labels",
+		stylers:[
+			{ visibility: "off" }
+		] 
+	},{
+		featureType: "administrative",
+		elementType: "all",
+		stylers: [
+			{ visibility: "on" }		
+		] 
+	},{
+		featureType: "landscape",
+		elementType: "all",
+		stylers: [
+			{ visibility: "on" }
+		] 
+	},{
+		featureType: "transit",			
+		elementType: "all",
+		stylers: [
+			{ visibility: "on" }			
+		] 
+	}];
+
+	//icon
+	var icon = {
+		url : "img/icon.png",
+		scaledSize : new google.maps.Size(48, 48)
+	}
+
+	var markerOptions = {
+	  	position: latlng,
+       		map: map,
+	  	icon: icon,
+      		title: 'Burger Vacation'
+	};
+	 
+     	//var marker = new google.maps.Marker(markerOptions);
+     	new google.maps.Marker(markerOptions);
+	var styledMapOptions = { name: 'Burger Vacation' }
+      	var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
+	
+	map.mapTypes.set('sample', sampleType);
+  	map.setMapTypeId('sample');
+	google.maps.event.addDomListener(window, 'load', initialize);
 }
