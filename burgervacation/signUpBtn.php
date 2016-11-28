3d403ac815cd98c7d7604528d5b7520f1539b82d
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2016-11-27 20:56:50 -0800

    access pdo

--- burgervacation/signUpBtn.php
+++ burgervacation/signUpBtn.php
@@ -1,12 +1,13 @@
 <?php
-//require "../burgerVacationMySql.php";
 require "../../mysql.php";
+
 if (isset($_POST["signUpBtn"])) {
-        $userName = $_POST['userName'];
+	
+	$userName = $_POST['userName'];
         $phoneNumber = $_POST['phoneNumber'];
         $email = $_POST['email'];
 	$password = $_POST['password'];
-	$registeredDate = date("Y/m/d");
+	$date = date("Y/m/d");
         $errorMessage ='';
         $flg = 0;
 
@@ -34,26 +35,20 @@ if (isset($_POST["signUpBtn"])) {
 		$user = 'LAA0786421';
 		$pass = "danielpowter";
 		try{
-			$pdo = new PDO('mysql:host=mysql108.phy.lolipop.lan;dbname=LAA0786421-hiroshi;charset=utf8','LAA0786421','danielpowter', array(PDO::ATTR_EMULATE_PREPARES => false));
+			// local
+			// my server
+			// read "../../mysql.php" : $pdo = ~
+			$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-			/*
-			$stmt = $pdo -> prepare("INSERT INTO users (userName, phoneNumber, email, password, registeredDate) VALUES (:userName, :phoneNumber, :email, :password, :registeredDate)");
+			$stmt = $pdo -> prepare("INSERT INTO users (userName, phoneNumber, email, password, date) VALUES (:userName, :phoneNumber, :email, :password, :date)");
+			
 			$stmt->bindParam(':userName', $userName, PDO::PARAM_STR);
-			$stmt->bindValue(':phoneNumber', $phoneNumber, PDO::PARAM_INT);
+			$stmt->bindValue(':phoneNumber', $phoneNumber, PDO::PARAM_STR);
 			$stmt->bindParam(':email', $email, PDO::PARAM_STR);
 			$stmt->bindParam(':password', $password, PDO::PARAM_STR);
-			$stmt->bindParam(':registeredDate', $registeredDate, PDO::PARAM_STR);
+			$stmt->bindParam(':date', $date, PDO::PARAM_STR);
 			$stmt->execute();
-			 */
-
-			/*
-			$dbh = new PDO($dsn,$user,$pass);
-			$sql = 'INSERT INTO users(userName, phoneNumber, email, password) VALUES(?, ?, ?, ?)';
 			
-			$arr = [$userName, $phoneNumber, $email, $password];
-			$stmt = $dbh->prepare($sql);
-			$stmt->execute($arr);
-			 */
 		}catch(PDOException $e){
 			header('Content-Type: text/plain; charset=UTF-8', true, 500);
 			exit($e->getMessage());
